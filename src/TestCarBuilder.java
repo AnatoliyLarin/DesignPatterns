1b5da9320e94a309f729145b05722e5f06118201
anatolii <anatoliy.larin@outlook.com>
2018-12-06 22:08:54 +0200

    5-th - Builder pattern

--- src/TestCarBuilder.java
+++ src/TestCarBuilder.java
@@ -23,7 +23,7 @@ public class TestCarBuilder {
 
         System.out.println("\nOrdered car's info\n");
         System.out.println("Gear type: " + car.getGearType().run() + "\n" + "Wheel type: " + car.getWheelType().run());
-        System.out.println("Break beak: " + car.getBreakType().stop() + "\n" + "Color: " + car.getColor());
+        System.out.println("Break type: " + car.getBreakType().stop() + "\n" + "Color: " + car.getColor());
         System.out.println("Seats number: " + car.getNumberOfSeats() + "\n" + "Conditioning: " + car.isHasAirCondition());
         System.out.println("Max speed: " + car.getMaxSpeed() + "\n" + "Body's width/height: " + car.getWidth() + "/" + car.getHeight());
     }
