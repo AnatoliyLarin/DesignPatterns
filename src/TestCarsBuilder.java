36cab81e73083beb618830a7f656d5a5f64f980f
a.larin <anatoliy.larin@outlook.com>
2018-12-06 18:13:49 +0200

    5-th push: builder

--- src/TestCarsBuilder.java
+++ src/TestCarsBuilder.java
@@ -8,9 +8,19 @@ public class TestCarsBuilder {
     public static void main(String[] args){
 
         CarsBuilder carsBuilder = new CarsBuilder();
-        AssembleLine produce = new AssembleLine();
         Context factory = new Context();
 
-        Cars car = carsBuilder.setBreakType(factory.getFactory("Tesla").createBreak(("Soft")));
+        Cars car = carsBuilder.setBreakType(factory.getFactory("Tesla").createBreak(("Soft")))
+                .setColor("Green")
+                .setHasAirCondition(true)
+                .setGearType(factory.getFactory("Tesla").createGear(("Hard")))
+                .setWheelType(factory.getFactory("Tesla").createWheel(("Winter")))
+                .setHeight(175)
+                .setWidth(350)
+                .setMaxSpeed(175)
+                .setNumberOfSeats(7)
+                .build();
+
+            System.out.println("Car's color: " + car.getColor() + "\n" + "Car's wheel type: " + car.getWheelType());
     }
 }
